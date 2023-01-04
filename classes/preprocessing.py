0a9da2ebeb34d80997d58a3cb9eb884216ef181e
magda <maddabrowska@gmail.com>
2023-01-04 18:09:02 +0100

    fix save file and color extraction

--- classes/preprocessing.py
+++ classes/preprocessing.py
@@ -1,35 +1,60 @@
-import cv2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 14:16:14 2022
 
+@author: Magda
+"""
+import cv2
 import numpy as np
 
-capture = cv2.VideoCapture('C:\\Users\\Magda\\Downloads\\box.MP4')
-video_output = cv2.VideoWriter('C:\\Users\\Magda\\Downloads\\box.MP4',
-                               cv2.VideoWriter_fourcc(*'mp4v'),
-                               10,
-                               (640, 480))
-frameNr = 0
+film = cv2.VideoCapture(
+    'C:\\Users\\Magda\\Desktop\\Magda nauka\\Informatyka\\Semestr 5\\Uczenie maszynowe\\box.MP4')
+w = int(film.get(3))
+h = int(film.get(4))
 
-while True:
-    success, frame = capture.read(0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    light_blue = np.array([110, 50, 50])
-    dark_blue = np.array([130, 255, 255])
-    mask = cv2.inRange(hsv, light_blue, dark_blue)
-    output = cv2.bitwise_and(frame, frame, mask=mask)
-    video_output.write(output)
-    cv2.imshow('output', output)
-    if cv2.waitKey(1) & 0xFF == ord('Q'):
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+out = cv2.VideoWriter('C:\\Users\\Magda\\Desktop\\Magda nauka\\Informatyka\\Semestr 5\\Uczenie maszynowe\\extracted.mp4', fourcc, 50.0, (w, h), True)
+
+bgSubstractor = cv2.createBackgroundSubtractorMOG2()
+kernel_o = np.ones((6,6),np.uint8)
+kernel_c = np.ones((10,10),np.uint8)
+
+while (True):
+    bul, klatka = film.read()
+    if bul:
+        height, width, layers = klatka.shape
+
+        klatka_hsv = cv2.cvtColor(klatka, cv2.COLOR_BGR2HSV)
+        low_red1 = np.array([169, 170, 20])
+        high_red1 = np.array([179, 255, 255])
+        red_mask1 = cv2.inRange(klatka_hsv, low_red1, high_red1)
+
+        low_red2 = np.array([0, 170, 20])
+        high_red2 = np.array([9, 255, 255])
+        red_mask2 = cv2.inRange(klatka_hsv, low_red2, high_red2)
+
+        red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+
+        low_blue = np.array([112, 170, 20])
+        high_blue = np.array([126, 255, 255])
+        blue_mask = cv2.inRange(klatka_hsv, low_blue, high_blue)
+
+        maska = blue_mask | red_mask1 | red_mask2
+        fgmask = bgSubstractor.apply(klatka_hsv)
+
+        klatka = cv2.bitwise_and(klatka, klatka, mask=maska&fgmask)
+
+        oczyszczonaKlatka = cv2.morphologyEx(cv2.morphologyEx(klatka, cv2.MORPH_OPEN, kernel_o), cv2.MORPH_CLOSE, kernel_c)
+
+        out.write(oczyszczonaKlatka)
+        cv2.imshow('oczyszczonaKlatka', oczyszczonaKlatka)
+        stopNumber = cv2.waitKey(5) & 0xFF
+        if stopNumber == 27:
+            break
+
+    else:
         break
 
-    # if success:
-    #     # cv2.imwrite(f"C:\\Users\\Magda\\Downloads\\pythonBox\\frame_{frameNr}.jpg", frame)
-    # else:
-    #     break
-
-    frameNr += 1
-
-# capture.release()
-video_output.release()
+film.release()
+out.release()
 cv2.destroyAllWindows()
-print("The video was successfully saved")
-
