192dbf66e6ea1027a4e78fcbcd0cbb59fa472a89
magda <maddabrowska@gmail.com>
2023-01-08 13:52:15 +0100

    change model, add data augmentation

--- model.py
+++ model.py
@@ -4,34 +4,49 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Conv2D, MaxPooling2D, Flatten, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Nadam
+import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 class Model():
-    def model_definition(self):
+    def model_definition(learning_rate):
+        data_augmentation = tf.keras.Sequential(
+            [
+                RandomFlip("horizontal_and_vertical",
+                           input_shape=(299,
+                                        299,
+                                        3)),
+                RandomRotation(0.1),
+                RandomZoom(0.1),
+            ]
+        )
 
-        model = tf.keras.Sequential()
+        model = tf.keras.Sequential([
+            data_augmentation,
+            Rescaling(1. / 255),
 
-        model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', input_shape=(299, 299, 3), activation='relu')) #Xception default
-        model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-        model.add(Dropout(0.2))
+            Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
 
-        model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-        model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Dropout(0.2))
+            Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
 
-        model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
-        model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Dropout(0.2))
+            Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
 
-        model.add(Flatten())
+            Flatten(),
 
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.2))
+            Dense(128, activation='relu'),
+            Dropout(0.2),
 
-        model.add(Dense(5, activation='softmax'))
+            Dense(len(classes), activation='softmax')
+        ])
 
-        model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                      # SparseCategoricalCrossentropy(from_logits=True)
+                      metrics=['accuracy'])
 
+        model.summary()
         return model
